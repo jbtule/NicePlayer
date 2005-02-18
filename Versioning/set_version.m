7286339e8727f246cef68df9262d4e8ba35f041d
qin <unknown>
2005-02-18 21:04:49 +0000

    Reverting, didn't work.

--- Versioning/set_version.m
+++ Versioning/set_version.m
@@ -37,10 +37,10 @@ int main (int argc, const char * argv[]) {
     }
 
     // try and set the requested key
-    [myDict setObject: [NSString stringWithCString:argv[4]] forKey: [NSString stringWithCString: argv[3]]];
+    [myDict setObject: [NSString stringWithCString:argv[3]] forKey: [NSString stringWithCString: argv[2]]];
 
     // Write requested value to file
-    [myDict writeToFile:[NSString stringWithCString:argv[2]] atomically:YES];
+    [myDict writeToFile:[NSString stringWithCString:argv[1]] atomically:YES];
 
     [pool release];
     return 0;
