afa898350294022779be6bf204edbbfd273b4d26
tuley <unknown>
2005-02-19 00:46:37 +0000

    the open and quit worked better when they weren't overrided

--- Classes/Scripting/NiceAppScripting.m
+++ Classes/Scripting/NiceAppScripting.m
@@ -11,22 +11,6 @@
 /* To enable AE debugging: defaults write NSGlobalDomain NSScriptingDebugLogLevel 1 */
 @implementation NPApplication (NiceAppScripting)
 
-/* The default NSApp implementation is bogus when it comes to supporting AS... strange. */
--(void)handleOpenScriptCommand:(id)sender
-{
-	if([[sender directParameter] isKindOfClass:[NSArray class]]){
-		[[NiceController controller] openFiles:[sender directParameter]];
-	} else {
-		NSURL *newURL = (NSURL *)CFURLCreateWithFileSystemPath(NULL, (CFStringRef)[sender directParameter], 
-															   kCFURLHFSPathStyle, NO);
-		[[NiceController controller] openFiles:[NSArray arrayWithObject:[newURL path]]];
-		[newURL release];
-	}
-}
 
--(void)handleQuitScriptCommand:(id)sender
-{
-	[self terminate:self];
-}
 
 @end
