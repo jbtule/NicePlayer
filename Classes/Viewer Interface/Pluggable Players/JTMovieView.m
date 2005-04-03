f85230d3ce9128d9801c47892274a527547c664a
tuley <unknown>
2005-04-03 21:11:15 +0000

    Refactored IndyKit to use STEnum

--- Classes/Viewer Interface/Pluggable Players/JTMovieView.m
+++ Classes/Viewer Interface/Pluggable Players/JTMovieView.m
@@ -1,5 +1,5 @@
 /**
- * JTMovieView.m
+* JTMovieView.m
  * NicePlayer
  *
  * An implementation of a Quicktime view for NicePlayer.
@@ -13,41 +13,41 @@
 
 +(id)blankImage
 {
-	return [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"black" ofType:@"png"]];
+    return [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"black" ofType:@"png"]];
 }
 
 +(NSDictionary *)plugInfo
 {
-	NSArray *extensions = [NSArray arrayWithObjects:
-/* File extensions */
-		@"avi", @"mov", @"qt", @"mpg", @"mpeg", @"m15", @"m75", @"m2v", @"3gpp", @"mpg4", @"mp4", @"png",
-		@"gif", @"bmp", @"tif", @"pic", @"pct", @"pict", @"jpg", @"jpeg", @"qtif", @"swf", @"fli",
-		@"flc", @"dv", 
-/* Finder types */
-		@"VfW", @"MooV", @"MPEG", @"m2v ", @"mpg4", @"PNG ", @"GIF ", @"GiFf", @"BMP ", @"TIFF", @"PICT",
-		@"JPEG", @"qtif", @"SWFL", @"FLI ", @"dvc!",
-		nil];
-	return [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"Quicktime",	extensions,			nil]
-									   forKeys:[NSArray arrayWithObjects:@"Name",		@"FileExtensions",	nil]];	
+    NSArray *extensions = [NSArray arrayWithObjects:
+        /* File extensions */
+        @"avi", @"mov", @"qt", @"mpg", @"mpeg", @"m15", @"m75", @"m2v", @"3gpp", @"mpg4", @"mp4", @"png",
+        @"gif", @"bmp", @"tif", @"pic", @"pct", @"pict", @"jpg", @"jpeg", @"qtif", @"swf", @"fli",
+        @"flc", @"dv", 
+        /* Finder types */
+        @"VfW", @"MooV", @"MPEG", @"m2v ", @"mpg4", @"PNG ", @"GIF ", @"GiFf", @"BMP ", @"TIFF", @"PICT",
+        @"JPEG", @"qtif", @"SWFL", @"FLI ", @"dvc!",
+        nil];
+    return [NSDictionary dictionaryWithObjects:[NSArray arrayWithObjects:@"Quicktime",	extensions,			nil]
+                                       forKeys:[NSArray arrayWithObjects:@"Name",		@"FileExtensions",	nil]];	
 }
 
 +(BOOL)hasConfigurableNib
 {
-	return NO;
+    return NO;
 }
 
 +(id)configureNibView
 {
-	return nil;
+    return nil;
 }
 
 -(BOOL)openURL:(NSURL *)url
 {
-	myURL = url;
-	film = [[movieCache objectForKey:url] retain];
-	if(film == NULL)
-		film = [[NSMovie alloc] initWithURL:url byReference:YES];
-	return (film) ? YES : NO;
+    myURL = url;
+    film = [[movieCache objectForKey:url] retain];
+    if(film == NULL)
+        film = [[NSMovie alloc] initWithURL:url byReference:YES];
+    return (film) ? YES : NO;
 }
 
 -(void)precacheURL:(NSURL*)url{
@@ -56,28 +56,28 @@
 
 -(id)initWithFrame:(NSRect)frame
 {
-	if(self = [super initWithFrame:frame]){
-		theFirstDraw =NO;
-		oldPlayState = STATE_INACTIVE;
-		[self setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
-		[self showController:NO adjustingSize:NO];
-		[self setEditable:NO];
-		muted = [super isMuted];
-		if(![self movie]){
-			id blank = [[NSBundle mainBundle] pathForResource:@"black" ofType:@"png"];
-			if(!blank)
-				return nil;
-			id blankURL = [NSURL fileURLWithPath:blank];
-			if(!blankURL)
-				return nil;
-			id movie = [[NSMovie alloc] initWithURL:blankURL byReference:YES];
-			if(!movie)
-				return nil;
-			[self setMovie:[movie autorelease]];
-		}
-	}
-	
-	return self;
+    if(self = [super initWithFrame:frame]){
+        theFirstDraw =NO;
+        oldPlayState = STATE_INACTIVE;
+        [self setAutoresizingMask:(NSViewWidthSizable | NSViewHeightSizable)];
+        [self showController:NO adjustingSize:NO];
+        [self setEditable:NO];
+        muted = [super isMuted];
+        if(![self movie]){
+            id blank = [[NSBundle mainBundle] pathForResource:@"black" ofType:@"png"];
+            if(!blank)
+                return nil;
+            id blankURL = [NSURL fileURLWithPath:blank];
+            if(!blankURL)
+                return nil;
+            id movie = [[NSMovie alloc] initWithURL:blankURL byReference:YES];
+            if(!movie)
+                return nil;
+            [self setMovie:[movie autorelease]];
+        }
+    }
+    
+    return self;
 }
 
 -(void)close
@@ -86,7 +86,7 @@
 
 -(void)loadMovie
 {
-	[self setMovie:[film autorelease]];
+    [self setMovie:[film autorelease]];
 }
 
 -(void)keyDown:(NSEvent *)anEvent
@@ -103,29 +103,29 @@
 
 -(void)drawMovieFrame
 {
-	[self setNeedsDisplay:YES];
+    [self setNeedsDisplay:YES];
 }
 
 -(NSSize)naturalSize
 {
-	Rect aRect;
-	GetMovieNaturalBoundsRect([[self movie] QTMovie], &aRect);
-	return NSMakeSize((float)(aRect.right - aRect.left),
-					  (float)(aRect.bottom - aRect.top));
+    Rect aRect;
+    GetMovieNaturalBoundsRect([[self movie] QTMovie], &aRect);
+    return NSMakeSize((float)(aRect.right - aRect.left),
+                      (float)(aRect.bottom - aRect.top));
 }
 
 -(void)setLoopMode:(NSQTMovieLoopMode)flag
 {
-	[super setLoopMode:flag];
+    [super setLoopMode:flag];
 }
 
 - (void)drawRect:(NSRect)aRect{
     if(!theFirstDraw){
-		[[NSGraphicsContext currentContext] saveGraphicsState];
-		[[NSColor blackColor] set];
-		NSRectFill(aRect);
-		[[NSGraphicsContext currentContext] restoreGraphicsState];
-		theFirstDraw=YES;
+        [[NSGraphicsContext currentContext] saveGraphicsState];
+        [[NSColor blackColor] set];
+        NSRectFill(aRect);
+        [[NSGraphicsContext currentContext] restoreGraphicsState];
+        theFirstDraw=YES;
     }
     [super drawRect:aRect];
 }
@@ -133,22 +133,22 @@
 
 -(BOOL)muted
 {
-	return muted;
+    return muted;
 }
 
 -(void)setMuted:(BOOL)aBOOL
 {
-	muted = aBOOL;
+    muted = aBOOL;
     [super setMuted:aBOOL];
 }
 
 -(float)volume
 {
-	float volume = [super volume];
-	if(volume < 0)
-		volume *= -1;
-	
-	return volume;
+    float volume = [super volume];
+    if(volume < 0)
+        volume *= -1;
+    
+    return volume;
 }
 
 -(void)setVolume:(float)aVolume
@@ -161,12 +161,12 @@
 
 -(BOOL)isPlaying
 {
-	return [super isPlaying];
+    return [super isPlaying];
 }
 
 -(void)start
 {
-	[super start:self];
+    [super start:self];
 }
 
 -(void)stop
@@ -176,73 +176,73 @@
 
 -(void)ffStart:(int)seconds
 {
-	if(oldPlayState == STATE_INACTIVE)
-		oldPlayState = [self isPlaying] ? STATE_PLAYING : STATE_STOPPED;
-	[self stop];
-	[self ffDo:seconds];
+    if(oldPlayState == STATE_INACTIVE)
+        oldPlayState = [self isPlaying] ? STATE_PLAYING : STATE_STOPPED;
+    [self stop];
+    [self ffDo:seconds];
 }
 
 -(void)ffDo:(int)seconds
 {
     [self incrementMovieTime:seconds inDirection:DIRECTION_FORWARD];
-	[self drawMovieFrame];
+    [self drawMovieFrame];
 }
 
 -(void)ffEnd
 {
-	if(oldPlayState == STATE_PLAYING)
-		[self start];
-	oldPlayState = STATE_INACTIVE;
+    if(oldPlayState == STATE_PLAYING)
+        [self start];
+    oldPlayState = STATE_INACTIVE;
 }
 
 -(void)rrStart:(int)seconds
 {
-	if(oldPlayState == STATE_INACTIVE)
-		oldPlayState = [self isPlaying] ? STATE_PLAYING : STATE_STOPPED;
-	[self stop];
-	[self rrDo:seconds];
+    if(oldPlayState == STATE_INACTIVE)
+        oldPlayState = [self isPlaying] ? STATE_PLAYING : STATE_STOPPED;
+    [self stop];
+    [self rrDo:seconds];
 }
 
 -(void)rrDo:(int)seconds
 {
     [self incrementMovieTime:seconds inDirection:DIRECTION_BACKWARD];
-	[self drawMovieFrame];
+    [self drawMovieFrame];
 }
 
 -(void)rrEnd
 {
-	if(oldPlayState == STATE_PLAYING)
-		[self start];
-	oldPlayState = STATE_INACTIVE;
+    if(oldPlayState == STATE_PLAYING)
+        [self start];
+    oldPlayState = STATE_INACTIVE;
 }
 
 /* For QuickTime, a positive value is forward, a negative value is backward. */
 -(void)stepBackward
 {
-	[self stepFrameInDirection:-1];
+    [self stepFrameInDirection:-1];
 }
 
 -(void)stepForward
 {
-	[self stepFrameInDirection:1];
+    [self stepFrameInDirection:1];
 }
 
 -(void)stepFrameInDirection:(int)aDirection
 {
-	OSType myTypes[1];
-	Movie tempMovie = [[self movie] QTMovie];
+    OSType myTypes[1];
+    Movie tempMovie = [[self movie] QTMovie];
     TimeRecord tempRecord;
-	TimeValue newTime;
+    TimeValue newTime;
     TimeValue tempTime = GetMovieTime(tempMovie, &tempRecord);
     myTypes[0] =VisualMediaCharacteristic;      // we want video samples
     GetMovieNextInterestingTime(tempMovie, nextTimeStep, 1, myTypes, tempTime, aDirection, &newTime, nil);
-	SetMovieTimeValue(tempMovie, newTime);
+    SetMovieTimeValue(tempMovie, newTime);
 }
 
 -(BOOL)hasEnded:(id)sender
 {
     Movie tempMovie = [[self movie] QTMovie];
-	
+    
     return IsMovieDone(tempMovie);
 }
 
@@ -251,12 +251,12 @@
 
 -(double)totalTime
 {
-	return (double)[self totalTimePrecise] / [self currentMovieTimeScale];
+    return (double)[self totalTimePrecise] / [self currentMovieTimeScale];
 }
 
 -(double)currentMovieTime
 {
-	
+    
     return (double)[self currentMovieTimePrecise] / [self currentMovieTimeScale];
 }
 
@@ -308,7 +308,7 @@
 
 -(void)incrementMovieTime:(long)timeDifference inDirection:(enum direction)aDirection;
 {
-	[self setCurrentMovieTime:([self currentMovieTime] + (aDirection * timeDifference))];
+    [self setCurrentMovieTime:([self currentMovieTime] + (aDirection * timeDifference))];
 }
 
 #pragma mark -
@@ -316,34 +316,34 @@
 
 -(id)menuPrefix
 {
-	return @"QT";
+    return @"QT";
 }
 
 -(id)menuTitle
 {
-	NSString *file = [[[myURL absoluteString] lastPathComponent] stringByDeletingPathExtension];
-	file = (NSString *)CFURLCreateStringByReplacingPercentEscapes(NULL, (CFStringRef)file, (CFStringRef)@"");
-	return [file autorelease];
+    NSString *file = [[[myURL absoluteString] lastPathComponent] stringByDeletingPathExtension];
+    file = (NSString *)CFURLCreateStringByReplacingPercentEscapes(NULL, (CFStringRef)file, (CFStringRef)@"");
+    return [file autorelease];
 }
 
 -(id)pluginMenu
 {
-	id pluginMenu = [[NSMutableArray array] retain];
-	id newItem;
-	
-	newItem = [[[NSMenuItem alloc] initWithTitle:@"Play Movie Preview"
-										  action:@selector(playMoviePreview)
-								   keyEquivalent:@""] autorelease];
-	[newItem setTarget:self];
-	[pluginMenu addObject:newItem];
-
-	newItem = [[[NSMenuItem alloc] initWithTitle:@"Go to Movie Poster Frame"
-										  action:@selector(gotoMoviePosterFrame)
-								   keyEquivalent:@""] autorelease];
-	[newItem setTarget:self];
-	[pluginMenu addObject:newItem];
-	
-	return [pluginMenu autorelease];
+    id pluginMenu = [[NSMutableArray array] retain];
+    id newItem;
+    
+    newItem = [[[NSMenuItem alloc] initWithTitle:@"Play Movie Preview"
+                                          action:@selector(playMoviePreview)
+                                   keyEquivalent:@""] autorelease];
+    [newItem setTarget:self];
+    [pluginMenu addObject:newItem];
+    
+    newItem = [[[NSMenuItem alloc] initWithTitle:@"Go to Movie Poster Frame"
+                                          action:@selector(gotoMoviePosterFrame)
+                                   keyEquivalent:@""] autorelease];
+    [newItem setTarget:self];
+    [pluginMenu addObject:newItem];
+    
+    return [pluginMenu autorelease];
 }
 
 -(void)playMoviePreview
@@ -359,27 +359,27 @@
 /* Forward all drag events to the window itself. */
 -(NSDragOperation)draggingEntered:(id <NSDraggingInfo>)sender
 {
-	return [[self window] draggingEntered:sender];
+    return [[self window] draggingEntered:sender];
 }
 
 -(NSDragOperation)draggingUpdated:(id)sender
 {
-	return [[self window] draggingUpdated:sender];
+    return [[self window] draggingUpdated:sender];
 }
 
 -(BOOL)prepareForDragOperation:(id)sender
 {
-	return [[self window] prepareForDragOperation:sender];
+    return [[self window] prepareForDragOperation:sender];
 }
 
 -(BOOL)performDragOperation:(id <NSDraggingInfo>)sender
 {
-	return [[self window] performDragOperation:sender];
+    return [[self window] performDragOperation:sender];
 }
 
 -(void)concludeDragOperation:(id <NSDraggingInfo>)sender
 {
-	[[self window] concludeDragOperation:sender];
+    [[self window] concludeDragOperation:sender];
 }
 
 @end
