c5678ff9ae0c00196b5b53e1dfb48148b52f12b0
tuley <unknown>
2005-04-11 04:57:47 +0000

    Drawers are evil

--- Classes/NiceWindow/NiceDrawer.m
+++ Classes/NiceWindow/NiceDrawer.m
@@ -8,14 +8,20 @@
 
 #import "NiceDrawer.h"
 
+/*************************
+Drawers are evil private children, and aren't like normal children thus
+they need there own private methods, 
+and they ignore thier public  ones they inherited from their super.
+**************************/
+@interface NSDrawerWindow : NSWindow 
+    -(NSWindow*)_parentWindow;
+@end
 
 @implementation NiceDrawer
 
 - (void)mouseMoved:(NSEvent *)anEvent
 {
-	/* TODO: When Apple fixes the parent window drawer bug, fix this to not use
-				undocumented API. */
-	id theWindow = [[self window] _parentWindow];
+	id theWindow = [(NSDrawerWindow*)[self window] _parentWindow];
 	// create new event with useful window, location and event values
 	unsigned int flags = [anEvent modifierFlags];
 	NSTimeInterval timestamp = [anEvent timestamp];
