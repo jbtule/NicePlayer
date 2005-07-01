3e06816bf16ae11560cf9b44ebaf78e0e67ebbab
qin <unknown>
2005-07-01 06:41:21 +0000

    Forgot to add some files.

---
+++ Plugin Sources/DVDPlayer/NSBookmarkCreateButton.h
@@ -0,0 +1,18 @@
+//
+//  NSBookmarkCreateButton.h
+//  DVDPlayer
+//
+//  Created by Robert Chin on 6/30/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface NSBookmarkCreateButton : NSButton {
+	IBOutlet id entryText;
+}
+
+-(id)entryText;
+
+@end
---
+++ Plugin Sources/DVDPlayer/NSBookmarkCreateButton.m
@@ -0,0 +1,19 @@
+//
+//  NSBookmarkCreateButton.m
+//  DVDPlayer
+//
+//  Created by Robert Chin on 6/30/05.
+//  Copyright 2005 __MyCompanyName__. All rights reserved.
+//
+
+#import "NSBookmarkCreateButton.h"
+
+
+@implementation NSBookmarkCreateButton
+
+-(id)entryText
+{
+	return [entryText stringValue];
+}
+
+@end
